5ce33ee9d251096fe28ae5e58a9717d49c56b6e5
Luke Gill <bagrad007@gmail.com>
2021-01-29 17:26:55 -0500

    adds error handling for invalid zip

--- lib/api.rb
+++ lib/api.rb
@@ -1,10 +1,13 @@
 class Api
-@@api_key = "2e5a7bbe0a508c6a52f4083839ea0eb5"
+  @@api_key = "2e5a7bbe0a508c6a52f4083839ea0eb5"
 
-def self.get_weather_by_zip(zip)
-    @@url = "api.openweathermap.org/data/2.5/weather?zip=#{zip},us&units=imperial&appid=#{@@api_key}"
-       response = HTTParty.get(@@url)
-       weather_hash = {name: response["name"], wind_speed: response["wind"]["speed"], temp: response["main"]["temp"]}
-       Location.new(weather_hash)
+  def self.get_weather_by_zip(zip)
+    url = "api.openweathermap.org/data/2.5/weather?zip=#{zip},us&units=imperial&appid=#{@@api_key}"
+    response = HTTParty.get(url)
+    if response["message"]
+      return nil
     end
+    weather_hash = { name: response["name"], wind_speed: response["wind"]["speed"], temp: response["main"]["temp"] }
+    Location.new(weather_hash)
+  end
 end
--- lib/cli.rb
+++ lib/cli.rb
@@ -1,24 +1,44 @@
 class Cli
-    def welcome
-        puts "Welcome to the weather CLI app!"
-        self.prompt_for_zip
+  def welcome
+    puts "Welcome to the weather CLI app!"
+    self.prompt_for_zip
+  end
+
+  def prompt_for_zip
+    puts "Please enter your zip code"
+    input = gets.strip
+    new_location = Api.get_weather_by_zip(input)
+    if new_location
+      self.weather_options(new_location)
+    else
+      puts "That zip code was invalid."
+      self.prompt_for_zip
     end
+  end
 
-    def prompt_for_zip
-        puts "Please enter your zip code"
-        input = gets.strip
-        new_location = Api.get_weather_by_zip(input)
-        self.weather_options(new_location)
+  def weather_options(location)
+    puts "Please choose from the following list for weather information for #{location.name}"
+    puts "1. Windspeed"
+    puts "2. Temp"
+    puts "3. Feels Like"
+    puts "4. Cloud Cover"
+    self.user_selection(location)
+  end
+
+  def user_selection(location)
+    input = gets.strip
+
+    if input == "1"
+      puts "The wind speed in #{location.name} is #{location.wind_speed}"
+    elsif input == "2"
+      puts "The temperature in #{location.name} is #{location.temp}"
+    elsif input == "3"
+      puts "In #{location.name} it feels like #{location.feels_like}"
+    elsif input == "4"
+      "The cloud cover in #{location.name} is #{location.cloud_cover}"
+    else
+      puts "That selection was invalid"
+      self.weather_options(location)
     end
-
-    def weather_options(location)
-        puts "Please choose from the following list for weather information for #{location.name}"
-        puts "1. Windspeed"
-        puts "2. Temp"
-        puts "3. Feels Like"
-        puts "4. Cloud Cover"
-    end
-
-                
-
+  end
 end
