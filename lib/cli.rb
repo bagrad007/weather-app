58781729a0cd5a518d67efc2de90469bb8d5cd04
Luke Gill <bagrad007@gmail.com>
2021-01-29 11:46:59 -0500

    changes

--- lib/cli.rb
+++ lib/cli.rb
@@ -1,3 +1,14 @@
 class Cli
-    
+    def welcome
+        puts "Welcome to the weather CLI app!"
+        self.prompt_for_zip
+    end
+
+    def prompt_for_zip
+        puts "Please enter your zip code"
+        input = gets.strip
+    end
+
+                
+
 end
